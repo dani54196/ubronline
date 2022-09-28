1ea4321de3e368f88f9ae019d873025e87f3b59a
Daniel Cardozo <dani54196@gmail.com>
2022-09-28 12:53:40 -0300

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-232ceb945cebb37a7290.js"
+    "url": "webpack-runtime-127268ccdeb378ce1964.js"
   },
   {
     "url": "framework-5651cc7c6f7c057b0886.js"
   },
   {
-    "url": "app-0ca3fb0af91cc2b80ce2.js"
+    "url": "app-35572f30145856093ac1.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5f809ce02fe42f368b289afaad60ee5b"
+    "revision": "30d13c8947a21d5f612ed2b7bb5116cd"
   },
   {
     "url": "manifest.webmanifest",
@@ -151,7 +151,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`https://ubronline.com/app-0ca3fb0af91cc2b80ce2.js`))) {
+  if (!resources || !(await caches.match(`https://ubronline.com/app-35572f30145856093ac1.js`))) {
     return await fetch(event.request)
   }
 
