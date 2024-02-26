ad6671cd8c48f48d60e0a4544bd1293974c3fd6e
Daniel <dani54196@gmail.com>
2024-02-25 23:13:53 -0300

    fix seo

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  siteTitle: 'UBRO', // <title>
-  manifestName: 'UBRO',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'UBRO - Registro de mascotas', // <title>
+  manifestName: 'Universal Bull Registry Online',
+  manifestShortName: 'UBRO', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
+  manifestDisplay: 'Registro de Perros',
   manifestIcon: 'src/assets/img/website-icon.png',
   //pathPrefix: `/ubronline/`,  // This path is subpath of your hosting https://domain/portfolio
   heading: 'UBRO',
-  subHeading: 'Universal Bull Registry Online ',
+  subHeading: 'UBRO, REGISTRO DE MASCOTAS ONLINE',
   // social
 
-  phone: '0123456789',
-  address: 'Argentina',
+  phone: '+5492994021637',
+  address: 'Salta, Argentina',
 };
